a837e9f74740489351230606affaa8bca8fe5a3b
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-04-02 15:27:14 +0200

    chore: Remove static file service

--- main.go
+++ main.go
@@ -28,7 +28,6 @@ func main() {
 	router := gin.Default()
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
-	router.Static("/", "./public")
 	router.POST("/upload", func(c *gin.Context) {
 		var bindFile BindFile
 
